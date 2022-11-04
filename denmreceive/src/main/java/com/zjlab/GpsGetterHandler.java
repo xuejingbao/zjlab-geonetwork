3d15ec7b8567b70b9e0d34b01df4c8a1edaef35e
xue <2990056167@qq.com>
2022-11-04 17:00:30 +0800

    保证27长度才进行解析，防止tcp切片崩溃

--- denmreceive/src/main/java/com/zjlab/GpsGetterHandler.java
+++ denmreceive/src/main/java/com/zjlab/GpsGetterHandler.java
@@ -20,36 +20,37 @@ public class GpsGetterHandler extends SimpleChannelInboundHandler<ByteBuf> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
-        byte[] message = new byte[byteBuf.readableBytes()];
-        byteBuf.readBytes(message);
-        ByteBuf returnBuf = channelHandlerContext.alloc().buffer();
+        if (byteBuf.readableBytes() == 27) {
+            byte[] message = new byte[byteBuf.readableBytes()];
+            byteBuf.readBytes(message);
+            ByteBuf returnBuf = channelHandlerContext.alloc().buffer();
 
-        String json = new String(message, StandardCharsets.UTF_8);
-        List list = GsonUtil.stringToList(json, List.class).get(0);
-        Double lon = (double) list.get(0);
-        Double lat = (double) list.get(1);
-        if (log.isDebugEnabled()) {
-            log.debug("获取小车位置信息,经度: {},纬度: {}", lon, lat);
+            String json = new String(message, StandardCharsets.UTF_8);
+            List list = GsonUtil.stringToList(json, List.class).get(0);
+            Double lon = (double) list.get(0);
+            Double lat = (double) list.get(1);
+            if (log.isDebugEnabled()) {
+                log.debug("获取小车位置信息,经度: {},纬度: {}", lon, lat);
+            }
+            WarnMessage warnMessage = AreaJudge.INSTANCE.inside(new Position(lat, lon));
+            List reLs;
+            if (warnMessage == null) {
+                reLs = Arrays.asList(0);
+            } else {
+                log.warn("在警告区域内部，警告点是:{},警告半径:{},当前位置:{}"
+                        , warnMessage.getWarnPoint(), warnMessage.getWarnRadius(), warnMessage.getNowPoint());
+                reLs = Arrays.asList(
+                        1,
+                        warnMessage.getNowPoint().getLon(),
+                        warnMessage.getNowPoint().getLat(),
+                        warnMessage.getWarnPoint().getLon(),
+                        warnMessage.getWarnPoint().getLat(),
+                        warnMessage.getWarnRadius()
+                );
+            }
+            returnBuf.writeBytes(GsonUtil.object2String(reLs).getBytes(StandardCharsets.UTF_8));
+            channelHandlerContext.writeAndFlush(returnBuf);
         }
-        WarnMessage warnMessage = AreaJudge.INSTANCE.inside(new Position(lat, lon));
-        List reLs;
-        if (warnMessage == null) {
-            reLs = Arrays.asList(0);
-        } else {
-            log.warn("在警告区域内部，警告点是:{},警告半径:{},当前位置:{}"
-                    , warnMessage.getWarnPoint(), warnMessage.getWarnRadius(), warnMessage.getNowPoint());
-            reLs = Arrays.asList(
-                    1,
-                    warnMessage.getNowPoint().getLon(),
-                    warnMessage.getNowPoint().getLat(),
-                    warnMessage.getWarnPoint().getLon(),
-                    warnMessage.getWarnPoint().getLat(),
-                    warnMessage.getWarnRadius()
-            );
-        }
-        returnBuf.writeBytes(GsonUtil.object2String(reLs).getBytes(StandardCharsets.UTF_8));
-        channelHandlerContext.writeAndFlush(returnBuf);
-
     }
 
 
